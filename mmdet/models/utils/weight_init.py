eb9a523847536ad99366e4a40c19cc84b343cef6
Yamin05114 <liyamin0514@gmail.com>
2020-02-24 15:34:58 +0800

    Update weight_init.py

--- mmdet/models/utils/weight_init.py
+++ mmdet/models/utils/weight_init.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 # so many init, which one is the best? Kaiming, it is also the default init for every module.
-# pytorch have this rule: default is always the best, so if you don't config anything, kaiming is enabled.
+# pytorch have this rule: default is always the best, so if you don't config anything, kaiming uniform is enabled.
 def xavier_init(module, gain=1, bias=0, distribution='normal'):
     assert distribution in ['uniform', 'normal']
     if distribution == 'uniform':
