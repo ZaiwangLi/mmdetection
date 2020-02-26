9c30859f0333e89b2dd3661e385c0ed8945c813e
Yamin05114 <liyamin0514@gmail.com>
2020-02-26 15:42:14 +0800

    Update misc.py

--- mmdet/core/utils/misc.py
+++ mmdet/core/utils/misc.py
@@ -1,7 +1,12 @@
 # transpose + contiguous
 # *args: no default values
 # **kwargs: key value pair with defults
-# 
+# map: 
+# >>>def square(x) :            # 计算平方数
+# ...     return x ** 2
+# ... 
+# >>> map(square, [1,2,3,4,5])   # 计算列表各个元素的平方
+# [1, 4, 9, 16, 25]
 
 
 from functools import partial
