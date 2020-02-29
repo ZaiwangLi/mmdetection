d2cff3bb5ca34cb13303eec49b0ad0d20639c1b2
Yamin05114 <liyamin0514@gmail.com>
2020-02-29 17:17:43 +0800

    Update anchor_generator.py

--- mmdet/core/anchor/anchor_generator.py
+++ mmdet/core/anchor/anchor_generator.py
@@ -93,12 +93,23 @@ class AnchorGenerator(object):
         shift_x = torch.arange(0, feat_w, device=device) * stride
         shift_y = torch.arange(0, feat_h, device=device) * stride
         shift_xx, shift_yy = self._meshgrid(shift_x, shift_y)
+        # xx 1, 2, 3,1 2,3,1,2,3 yy 111222333
+        # 1,1   1,2   1,3 .... 
         shifts = torch.stack([shift_xx, shift_yy, shift_xx, shift_yy], dim=-1)
         shifts = shifts.type_as(base_anchors)
+        
+        
+        #  
         # first feat_w elements correspond to the first row of shifts
         # add A anchors (1, A, 4) to K shifts (K, 1, 4) to get
         # shifted anchors (K, A, 4), reshape to (K*A, 4)
-
+        # base anchors
+        #
+        #  x . . . . .      xxxxxx
+        #  . . . . . .      xxxxxx
+        #  . . . . . . =>   xxxxxx
+        #  . . . . . .      xxxxxx
+        #
         all_anchors = base_anchors[None, :, :] + shifts[:, None, :]
         all_anchors = all_anchors.view(-1, 4)
         # first A rows correspond to A anchors of (0, 0) in feature map,
